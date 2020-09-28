13763729b60db78e8abe2c1403dc6de079b827b4
Emanuel Ospino Gutierrez <oemanuel@uninorte.edu.co>
2020-09-28 16:28:09 -0500

    save name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
     def login 
         render '/user/Login'
     end
+    
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :birth, :email, :password, :password_confirmation) }
+    end
 end
