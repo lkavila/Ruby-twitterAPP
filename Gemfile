68986b50a995833ab7112c357f98abfd2cc7d33f
Emanuel Ospino Gutierrez <oemanuel@uninorte.edu.co>
2020-10-30 21:40:37 -0500

    test tweet with user FactoryBot

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   gem 'sqlite3', '~> 1.4'
   gem 'rspec-rails', '~> 4.0.1'
   gem 'shoulda-matchers', '~> 4.0'
+   gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -4,7 +4,19 @@ RSpec.describe TweetsController, "#create" do
     context "When a tweet is created with invalid params" do
         it "Should re-render the tweet form" do
             expect {Tweet.create!}.to raise_error(ActiveRecord::RecordInvalid)
-        end
-        
+        end  
     end
-  end
+    context "When user is logged in" do
+        let(:user){create(:user)}
+        before do
+            sign_in user
+            post :create, params: {tweet: {text: "This is a tweet by test",user_id: user.id}}
+        end
+        it "should create a new tweet" do
+            expect(Tweet.last.text).to eq("This is a tweet by test")
+        end
+        it "should redirect to Home" do
+            expect(subject).to redirect_to home_path
+        end
+    end
+end
